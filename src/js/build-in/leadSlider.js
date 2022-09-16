3e2113b5f83c4865fa382a644a4a305d4f5c90b8
Albert <kamalov00albert@gmail.com>
2022-09-16 14:54:20 +0300

    Адаптировал блок invitation и компонент lead-slider

--- src/js/build-in/leadSlider.js
+++ src/js/build-in/leadSlider.js
@@ -18,12 +18,21 @@ export default function leadSlider() {
         const navigationItems = slider.querySelectorAll('.lead-slider__navigation-item');
 
         navigationItems.forEach((navItem, index) => {
-            navItem.addEventListener('mouseenter', e => {
-                navigationItems.forEach(navItemEl => navItemEl.classList.remove('active'));
-                e.currentTarget.classList.add('active');
+            if (matchMedia('(max-width: 1024px)').matches) {
+                navItem.addEventListener('click', e => {
+                    navigationItems.forEach(navItemEl => navItemEl.classList.remove('active'));
+                    e.currentTarget.classList.add('active');
 
-                swiper.slideTo(index);
-            })
+                    swiper.slideTo(index);
+                })
+            } else {
+                navItem.addEventListener('mouseenter', e => {
+                    navigationItems.forEach(navItemEl => navItemEl.classList.remove('active'));
+                    e.currentTarget.classList.add('active');
+
+                    swiper.slideTo(index);
+                })
+            }
         })
     })
 }
