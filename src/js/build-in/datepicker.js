4a945cbd6be47a8671a75d48597aab484a287491
rook16rus <kamalov00albert@gmail.com>
2022-10-18 20:47:33 +0300

    Сверстал таб с курьером

--- src/js/build-in/datepicker.js
+++ src/js/build-in/datepicker.js
@@ -4,18 +4,50 @@ export default function datepicker() {
     const elementsDate = document.querySelectorAll('.js-datepicker');
 
     elementsDate.forEach(element => {
-       new AirDatepicker(element, {
+       const datepicker = new AirDatepicker(element, {
            dateFormat: 'd MMM',
            minDate: 0,
-       })
+           onSelect({date, formattedDate, datepicker}) {
+               const now = new Date();
+
+               if (date.getDate() === now.getDate() && date.getMonth() === now.getMonth() && date.getFullYear() === now.getFullYear()) {
+                    datepicker.$el.value = `Сегодня, ${datepicker.$el.value}`;
+               }
+           },
+           onShow(isFinished) {
+               if (isFinished) return;
+               rotateArrow(datepicker);
+           },
+           onHide(isFinished) {
+               if (isFinished) return;
+               rotateArrow(datepicker)
+           }
+       });
     });
 
     const elementsTime = document.querySelectorAll('.js-datepicker-time');
 
     elementsTime.forEach(element => {
-        new AirDatepicker(element, {
+        const datepicker = new AirDatepicker(element, {
             timepicker: true,
             onlyTimepicker: true,
+            range: true,
+            multipleDatesSeparator: ' - ',
+            onShow(isFinished) {
+                if (isFinished) return;
+                rotateArrow(datepicker);
+            },
+            onHide(isFinished) {
+                if (isFinished) return;
+                rotateArrow(datepicker)
+            }
         })
     })
+}
+
+function rotateArrow(datepicker) {
+    const datepickerEl = datepicker.$el;
+    const container = datepickerEl.closest('.js-date-picker-container');
+
+    container.classList.toggle('active');
 }
