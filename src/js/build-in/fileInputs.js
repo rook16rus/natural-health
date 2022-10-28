d2c2af3d07e7f4aa07dc2a4918f288257cbda70e
Albert <kamalov00albert@gmail.com>
2022-10-28 17:33:17 +0300

    Теперь dropzone находится в одной форме в другими полями

--- src/js/build-in/fileInputs.js
+++ src/js/build-in/fileInputs.js
@@ -13,86 +13,86 @@ export default function fileInputs() {
         const progressCount = container.querySelector('.js-dropzone-progress-count')
 
         const dropzone = new Dropzone(input, {
+            url: 'img',
             addRemoveLinks: true,
             maxFiles: TOTAL_LENGTH,
+            init() {
+                const showProgress = () => {
+                    if (checkFilesLength(1)) {
+                        progressBlock.classList.add('show');
+                    } else {
+                        progressBlock.classList.remove('show');
+                    }
+                }
+
+                const calculateProgress = () => {
+                    const length = this.files.length;
+                    const percent = (length * 100) / TOTAL_LENGTH;
+
+                    progress.style.setProperty('--percent', percent + '%');
+                    progressCount.textContent = length < 10 ? '0' + length : length;
+                }
+
+                const checkFormat = (file) => {
+                    const format = file.name.slice(-3);
+
+                    if (format === 'jpg' || format === 'png' || format === 'jpeg') {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+
+                const checkFilesLength = (length) => {
+                    if (this.files.length >= length) {
+                        return true
+                    } else {
+                        return false
+                    }
+                }
+
+                const hideButton = () => {
+                    const button = container.querySelector('.dz-message');
+
+                    if (checkFilesLength(TOTAL_LENGTH)) {
+                        button.classList.add('hide');
+                    } else {
+                        button.classList.remove('hide');
+                    }
+                }
+
+                const hideContent = () => {
+                    if (checkFilesLength(1)) {
+                        content.classList.add('hide');
+                    } else {
+                        content.classList.remove('hide');
+                    }
+                }
+
+                this.on('addedfile', (file) => {
+                    if (!checkFormat(file)) {
+                        this.removeFile(file);
+                    }
+
+                    hideButton();
+                    hideContent();
+                    calculateProgress();
+                    showProgress();
+                    modalFix();
+                })
+
+                this.on('removedfile', () => {
+                    hideButton();
+                    hideContent();
+                    calculateProgress();
+                    showProgress();
+                    modalFix();
+                })
+
+                this.on('maxfilesexceeded', (file) => {
+                    this.removeFile(file);
+                })
+            }
         })
-
-        calculateProgress();
-
-        dropzone.on('addedfile', (file) => {
-            if (!checkFormat(file)) {
-                dropzone.removeFile(file);
-            }
-
-            hideButton();
-            hideContent();
-            calculateProgress();
-            showProgress();
-            modalFix();
-        })
-
-        dropzone.on('removedfile', () => {
-            hideButton();
-            hideContent();
-            calculateProgress();
-            showProgress();
-            modalFix();
-        })
-
-        dropzone.on('maxfilesexceeded', (file) => {
-            dropzone.removeFile(file);
-        })
-
-        function showProgress() {
-            if (checkFilesLength(1)) {
-                progressBlock.classList.add('show');
-            } else {
-                progressBlock.classList.remove('show');
-            }
-        }
-
-        function calculateProgress() {
-            const length = dropzone.files.length;
-            const percent = (length * 100) / TOTAL_LENGTH;
-
-            progress.style.setProperty('--percent', percent + '%');
-            progressCount.textContent = length < 10 ? '0' + length : length;
-        }
-
-        function checkFormat(file) {
-            const format = file.name.slice(-3);
-
-            if (format === 'jpg' || format === 'png' || format === 'jpeg') {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        function checkFilesLength(length) {
-            if (dropzone.files.length >= length) {
-                return true
-            } else {
-                return false
-            }
-        }
-
-        function hideButton() {
-            const button = container.querySelector('.dz-message');
-
-            if (checkFilesLength(TOTAL_LENGTH)) {
-                button.classList.add('hide');
-            } else {
-                button.classList.remove('hide');
-            }
-        }
-
-        function hideContent() {
-            if (checkFilesLength(1)) {
-                content.classList.add('hide');
-            } else {
-                content.classList.remove('hide');
-            }
-        }
     })
 }
