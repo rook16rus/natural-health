cbf9e50e2b62177145c6b040f582a649be40f9ed
Albert <kamalov00albert@gmail.com>
2022-10-31 13:57:54 +0300

    По-другому реализовал кнопку "Читать полностью"

--- src/js/build-in/readMoreButton.js
+++ src/js/build-in/readMoreButton.js
@@ -1,17 +1,35 @@
 export default function readMoreButton() {
-    const content = document.querySelector('.js-read-content');
-    if (!content) return;
+    const buttons = document.querySelectorAll('.js-read-button');
 
-    const button = content.nextElementSibling;
-    if (!button.classList.contains('js-read-button')) return;
+    if (!matchMedia('(max-width: 640px)').matches) return
 
-    button.addEventListener('click', () => {
-        if (content.classList.contains('active')) {
-            button.textContent = 'Читать полностью';
-        } else {
-            button.textContent = 'Скрыть текст';
-        }
+    buttons.forEach(button => {
+        const container = button.closest('.wide-text-container');
+        if (!container) return
 
-        content.classList.toggle('active');
+        let buttonIndex;
+        const hideElements = [];
+
+        [...container.children].forEach((el, index) => {
+            if (el === button) buttonIndex = index;
+
+            if (buttonIndex && index > buttonIndex) {
+                el.classList.add('hide');
+                hideElements.push(el);
+
+                el.remove();
+                container.insertBefore(el, button);
+            }
+        })
+
+        button.addEventListener('click', () => {
+            if (hideElements.find(item => item.classList.contains('hide'))) {
+                button.textContent = 'Скрыть текст';
+            } else {
+                button.textContent = 'Читать полностью';
+            }
+
+            hideElements.forEach(el => el.classList.toggle('hide'))
+        })
     })
 }
