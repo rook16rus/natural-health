ba424b3fa56fc2d42ebdb727c892996c6f0d17d6
Albert <kamalov00albert@gmail.com>
2022-10-31 17:57:27 +0300

    Правки

--- src/js/build-in/fancybox.js
+++ src/js/build-in/fancybox.js
@@ -1,9 +1,20 @@
 import { Fancybox } from '@fancyapps/ui';
+import { disableBodyScroll as lockScroll, clearAllBodyScrollLocks as unlockScroll } from 'body-scroll-lock';
 
 export default function fancybox() {
     Fancybox.bind('[data-fancybox]', {
         Image: {
             zoom: false
         },
+        on: {
+            init() {
+                lockScroll(this, {
+                    reserveScrollBarGap: true,
+                });
+            },
+            closing() {
+                unlockScroll(this);
+            }
+        }
     })
 }
